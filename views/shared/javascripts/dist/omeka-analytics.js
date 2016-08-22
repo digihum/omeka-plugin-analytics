842bb9484a99d5856fa4eed160ca0a84ae8e73a1
Tim Hollies <tim.hollies@warwick.ac.uk>
2016-08-22 09:49:38 +0100

    add pause tracking

--- views/shared/javascripts/dist/omeka-analytics.js
+++ views/shared/javascripts/dist/omeka-analytics.js
@@ -11,10 +11,20 @@ ga('send', 'pageview');
 
 
 jQuery(document).ready(function() {
+
 	jQuery("audio").on("play", function(event) {
 		console.log("playing", event.target.currentSrc);
 		ga('send', 'event', {
 			eventCategory: 'Playing Audio',
+			eventAction: 'click',
+			eventLabel: event.target.currentSrc
+		});
+	});
+
+	jQuery("audio").on("pause", function(event) {
+		console.log("paused", event.target.currentSrc);
+		ga('send', 'event', {
+			eventCategory: 'Paused Audio',
 			eventAction: 'click',
 			eventLabel: event.target.currentSrc
 		});
